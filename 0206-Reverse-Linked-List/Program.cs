7ab97407d6356be5d6c815e3a33eb7007fe954ac
jinmin liu <86b26@sun.cis.scu.edu.tw>
2021-02-18 17:24:34 +0800

    add 206 question

---
+++ 0206-Reverse-Linked-List/Program.cs
@@ -0,0 +1,31 @@
+﻿using ShareLib;
+using System;
+
+namespace _0206_Reverse_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l1 = LinkedListHelper.ConvertToListNodes(new int[] { 1, 2, 3, 4, 5 });
+            l1 = ReverseList(l1);
+            LinkedListHelper.PrintListNodes(l1);
+            Console.ReadKey();
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                ListNode nextNode = current.next;
+                current.next = prev;
+                prev = current;
+                current = nextNode;
+            }
+            return prev;
+        }
+    }
+}
