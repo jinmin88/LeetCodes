6a466e61be606f297b949e0b53c5fdb029c34e58
jeff <jeff.liu@penpower.com.tw>
2021-07-20 23:42:31 +0800

    zigzag / dp / sort linked lsit

---
+++ 0006-ZigZag-Conversion/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace _0006_ZigZag_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.Convert("PAYPALISHIRING", 3));
+            Console.WriteLine(sol.Convert("PAYPALISHIRING", 4));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (s.Length <= numRows || numRows == 1)
+            {
+                return s;
+            }
+            var sb = new StringBuilder();
+            for (int i = 0; i<numRows; i++)
+            {
+                if (i == 0 || i == numRows - 1)
+                {
+                    int k = i;
+                    while (k < s.Length)
+                    {
+                        sb.Append(s[k]);
+                        k += (2 * numRows - 2);
+                    }
+                }
+                else
+                {
+                    bool is_down = true;
+                    int k = i;
+                    while (k < s.Length)
+                    {
+                        sb.Append(s[k]);
+                        if (is_down == true)
+                        {
+                            k += (numRows - i - 1) * 2;
+                            is_down = false;
+                        }
+                        else
+                        {
+                            k += 2 * i;
+                            is_down = true;
+                        }
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 0148-Sort-List/Program.cs
@@ -0,0 +1,60 @@
+﻿using ShareLib;
+using System;
+
+namespace _0148_Sort_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode SortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            ListNode mid = MidList(head);
+            ListNode left = SortList(head);
+            ListNode right = SortList(mid);
+            return Merge(left, right);
+        }
+
+        private ListNode MidList(ListNode head)
+        {
+            ListNode curr = null;
+            while (head != null && head.next != null)
+            {
+                if (curr == null)
+                {
+                    curr = head;
+                }
+                else
+                {
+                    curr = curr.next;
+                }
+                head = head.next.next;
+            }
+            ListNode mid = curr.next;
+            curr.next = null;
+            return mid;
+        }
+    
+        private ListNode Merge(ListNode left, ListNode right)
+        {
+            ListNode dummyHead = new ListNode();
+            ListNode tail = dummyHead;
+
+            while (left != null && right != null)
+            {
+                tail.next = left;
+            }
+        }
+    
+    
+    }
+
+}
