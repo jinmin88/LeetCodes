a90f910317f86ed146ff350cee0a2959f337d7f6
jinmin liu <86b26@sun.cis.scu.edu.tw>
2021-08-10 18:28:46 +0800

    chage solution

--- 0926-Flip-String-To-Monotone-Increasing/Program.cs
+++ 0926-Flip-String-To-Monotone-Increasing/Program.cs
@@ -18,42 +18,35 @@ namespace _0926_Flip_String_To_Monotone_Increasing
     {
         public int MinFlipsMonoIncr(string s)
         {
-            /*
-            dp0[0...n] = [0,0,0,...,0]
-            dp1[0...n] = [0,0,0,...,0]
-
-            if (s[i] == '0') {
-                //目前是0
-                dp1[i] = dp1[i-1] + 1;
-            }
-            else {
-                //目前是1
-                dp0[i] = dp0[i-1] + 1;
-            }
-            */
             int len = s.Length;
-            int[] dp0 = new int[len + 1];
-            int[] dp1 = new int[len + 1];
-            for (int i=0; i<len+1; i++)
+            int[][] dp = new int[len+1][];
+            for (int i=0; i<dp.Length; i++)
             {
-                dp0[i] = 0;
-                dp1[i] = 0;
+                dp[i] = new int[2];
             }
 
-            for (int i=0; i<len; i++)
+            for (int i=1; i<=len; i++)
             {
-                if (s[i] == '0')
-                {
-                    dp1[i + 1] = i == 0 ? 0 : dp1[i] + 1;
+                if (s[i-1] == '0')
+                {  
+                    
+                    //維持0的作法 => 讀到0不需要變化
+                    dp[i][0] = dp[i - 1][0];
+
+                    //變成最後一位是1的作法 => (維持目前是0序列 , 與最後幾位是1序列)兩者取小的+1(變自己)
+                    dp[i][1] = Math.Min(dp[i - 1][0], dp[i - 1][1]) + 1;
                 }
                 else
                 {
-                    dp0[i + 1] = i == 0 ? 0 : dp0[i] + 1;
+                    //維持0作法，把目前的1變0
+                    dp[i][0] = dp[i - 1][0] + 1;
+
+                    //變成最後一位是1的作法 => min(前面是0目前為1, 前面是1目前為1)
+                    dp[i][1] = Math.Min(dp[i - 1][0], dp[i - 1][1]);
                 }
             }
 
-
-            return Math.Min(dp0[len], dp1[len]);
+            return Math.Min(dp[len][0], dp[len][1]);
         }
     }
 }
