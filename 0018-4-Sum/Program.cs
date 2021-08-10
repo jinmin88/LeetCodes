f5f096854c966a679e9dd5aafa9a71c88c24ecd4
jeff <jeff.liu@penpower.com.tw>
2021-08-11 02:42:12 +0800

    2 q

---
+++ 0018-4-Sum/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0018_4_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            return kSum(nums, target, 0, 4);
+        }
+
+        private List<IList<int>> kSum(int[] nums, int target, int start, int k)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            if (start == nums.Length) //只剩一個元素可玩 => 無解
+                return res;
+            else if (nums[start] * k > target)  //最小的值乘上k倍大於target => 無解
+                return res;
+            else if (target > nums[nums.Length - 1] * k)  //target大於此陣列最大的數乘上k次=>一定無解
+                return res;
+
+            if (k == 2) return TwoSum(nums, target, start);
+
+            // |start|
+            for (int i = start; i<nums.Length; i++)
+            {
+                if (i == start || nums[i-1] != nums[i])
+                {
+                    //降k
+                    foreach (var subset in kSum(nums, target - nums[i], i + 1, k - 1))
+                    {
+                        List<int> innerResult = new List<int>() { nums[i] };
+                        innerResult.AddRange(subset);
+                        res.Add(innerResult);
+                    }
+                }
+            }
+            return res;
+        }
+
+        private List<IList<int>> TwoSum(int[] nums, int target, int start)
+        {
+            //  start ... lo-1   lo
+            //              x     x
+            
+            List<IList<int>> res = new List<IList<int>>();
+            int lo = start, hi = nums.Length - 1;
+            while (lo < hi)
+            {
+                int currSum = nums[lo] + nums[hi];
+                if (currSum < target || (lo > start && nums[lo] == nums[lo-1]))
+                {
+                    lo++;
+                }
+                else if (currSum > target || (hi < nums.Length-1 && nums[hi] == nums[hi+1]))
+                {
+                    hi--;
+                }
+                else
+                {
+                    res.Add(new List<int> { nums[lo], nums[hi] });
+                    lo++;
+                    hi--;
+                }
+            }
+            return res;
+        }
+
+
+    }
+
+
+}
---
+++ 0208-Implement-Trie/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace _0208_Implement_Trie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trie trie = new Trie();
+            trie.Insert("apple");
+            var s1 = trie.Search("apple");   // return True
+            var s2 = trie.Search("app");     // return False
+            var s3 = trie.StartsWith("app"); // return True
+            trie.Insert("app");
+            var s4 = trie.Search("app");     // return True
+        }
+    }
+
+    public class Trie
+    {
+        private readonly TrieNode root;
+
+        /** Initialize your data structure here. */
+        public Trie()
+        {
+            root = new TrieNode();
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert(string word)
+        {
+            if (string.IsNullOrEmpty(word)) 
+                return;
+            var curr = root;
+            foreach (var c in word)
+            {
+                if (curr.children[c - 'a'] == null)
+                {
+                    curr.children[c - 'a'] = new TrieNode();
+                }
+                curr = curr.children[c - 'a'];
+            }
+            curr.IsWord = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search(string word)
+        {
+            var curr = root;
+            foreach (var c in word)
+            {
+                if (curr.children[c - 'a'] == null)
+                    return false;
+                else
+                {
+                    curr = curr.children[c - 'a'];
+                }
+            }
+            return curr.IsWord;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith(string prefix)
+        {
+            var curr = root;
+            foreach (var c in prefix)
+            {
+                if (curr.children[c - 'a'] == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    curr = curr.children[c - 'a'];
+                }
+            }
+            return true;
+        }
+
+        private class TrieNode
+        {
+            public TrieNode()
+            {
+                IsWord = false;
+                for (int i=0; i<children.Length; i++)
+                {
+                    children[i] = null;
+                };
+            }
+
+            public bool IsWord { get; set; }
+
+            public TrieNode[] children = new TrieNode[26];
+
+        }
+
+
+    }
+
+}
