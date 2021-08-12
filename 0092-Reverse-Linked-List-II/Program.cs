5ceca503695b3125ce7e6fd65939446a4ab9a773
jinmin liu <86b26@sun.cis.scu.edu.tw>
2021-08-12 13:35:30 +0800

    0092/0106

---
+++ 0092-Reverse-Linked-List-II/Program.cs
@@ -0,0 +1,60 @@
+﻿using ShareLib;
+using System;
+
+namespace _0092_Reverse_Linked_List_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            if (head == null)
+                return null;
+
+            ListNode curr = head;
+            ListNode prev = null;
+
+            //move pointer to the first location need reverse
+            while (m > 1)
+            {
+                prev = curr;
+                curr = curr.next;
+                m--;
+                n--;
+            }
+
+            ListNode con = prev;
+            ListNode tail = curr;
+            ListNode next;
+
+            while (n > 0)
+            {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+                n--;
+            }
+
+            if (con != null)
+            {
+                con.next = prev;
+            }
+            else
+            {
+                head = prev;
+            }
+
+            tail.next = curr;
+            return head;
+
+        }
+    }
+}
